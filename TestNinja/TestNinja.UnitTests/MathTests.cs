e8b018dc8e1d7c23fa578660eae2c380e98a7949
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-01-02 22:30:04 +0600

    Testing Arrays and Collections

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -20,7 +20,6 @@ namespace TestNinja.UnitTests
         //[Ignore("I wanted to")]
         public void Add_WhenCalled_ReturnTheSUMofArgunents()
         {
-            _math = new Math();
             var result = _math.Add(1, 2);
             Assert.That(result,Is.EqualTo(3));
         }
@@ -30,9 +29,26 @@ namespace TestNinja.UnitTests
         [TestCase(1, 1, 1)]
         public void Add_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedReturn)
         {
-            _math = new Math();
             var result = _math.Add(a, b);
             Assert.That(result, Is.EqualTo(expectedReturn));
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
+        {
+            var result = _math.GetOddNumbers(5);
+            // Assert.That(result, Is.Not.Empty);
+
+            //Assert.That(result, Is.EqualTo(3));
+
+            //Assert.That(result, Does.Contain(1));
+            //Assert.That(result, Does.Contain(3));
+            //Assert.That(result, Does.Contain(5));
+
+            Assert.That(result, Is.EquivalentTo(new [] {1,3,5}));
+
+            //Assert.That(result, Is.Ordered);
+            //Assert.That(result, Is.Unique);
         }
 
     }
