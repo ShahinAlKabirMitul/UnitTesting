06b7add63a3c842b7c45edfb14dcd457250ac155
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-01-05 20:27:18 +0600

    Solution- DemeritPointsCalculator

---
+++ TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+using Math = System.Math;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _demeritPointsCalculator;
+        // Setup
+        // TearDown
+
+        [SetUp]
+        public void Setup()
+        {
+            _demeritPointsCalculator = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsOutofRange_ThrowArgumentOutofRangeException(int speed)
+        {
+            Assert.That( ()=> _demeritPointsCalculator.CalculateDemeritPoints(speed),Throws.Exception.TypeOf<ArgumentOutOfRangeException>() );
+        }
+
+       
+
+        [Test]
+        [TestCase(0,0)]
+        [TestCase(64,0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritPoints_WhenCall_ReturnDemeritPoints(int speed,int expectedResult)
+        {
+            var result = _demeritPointsCalculator.CalculateDemeritPoints(speed);
+            Assert.That(result,Is.EqualTo(expectedResult));
+        }
+       
+    }
+}
